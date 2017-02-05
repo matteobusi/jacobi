9dae8d0b48333533993c0912b1c7c4ade169f646
mcaos <caos750@gmail.com>
2017-02-05 21:21:39 +0100

    Sequential and pre-design

---
+++ JacobiSeq/cmake-build-debug/CMakeFiles/JacobiSeq.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JacobiSeq.dir/main.cpp.o"
+  "JacobiSeq.pdb"
+  "JacobiSeq"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JacobiSeq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
