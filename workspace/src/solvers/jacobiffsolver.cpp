7390068ad851b3a0a38c58af8bc47a8204199b10
mcaos <caos750@gmail.com>
2017-06-07 18:37:51 +0200

    FF fixed bug in num workers

--- workspace/src/solvers/jacobiffsolver.cpp
+++ workspace/src/solvers/jacobiffsolver.cpp
@@ -7,9 +7,10 @@
 void JacobiFFSolver::deltax(const double* x, double *dest)
 {
     // D^-1 ( b - Ax )
-    ff::ParallelFor pf(mnWorkers, false);
+    int grain = 10;
+    ff::ParallelFor pf(mnWorkers, true);
 
-    pf.parallel_for(0, mN, 1, 1, [&](const long i)
+    pf.parallel_for(0, mN, 1, grain, [&](const long i)
     {
         double s = 0.f;
         for (int j=0; j < i; j++)
@@ -18,5 +19,5 @@ void JacobiFFSolver::deltax(const double* x, double *dest)
         for (int j=i+1; j < mN; j++)
             s+= mA[i][j]*x[j];
         dest[i] = (mb[i] - s)/mA[i][i];
-    });
+    }, mnWorkers);
 }
