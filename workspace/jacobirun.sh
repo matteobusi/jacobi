4f701885b7c057a56afb8189d06c6b613e86813d
mcaos <caos750@gmail.com>
2017-06-07 19:10:04 +0200

    Fixed jacobirun

--- workspace/jacobirun.sh
+++ workspace/jacobirun.sh
@@ -4,7 +4,7 @@
 MAX_ITER=100
 EPS=0.001
 N_VALS=(5000 10000 15000 30000)
-HEADER = "nw,it,comp_t,upd_t,conv_t,latency,error"
+HEADER="nw,it,comp_t,upd_t,conv_t,latency,error"
 
 # WORKERS SETTINGS
 if [[ "$1" == "MIC" ]]
@@ -39,20 +39,20 @@ do
     echo $HEADER  > $RES_SEQ_FILE
     if [[ "$1" == "MIC" ]]
     then
-	ssh mic1 "bin/jacobim $N $MAX_ITER $EPS s" >> $RES_SEQ_FILE
+	ssh mic1 "./jacobim $N $MAX_ITER $EPS s" >> $RES_SEQ_FILE
     else
 	bin/jacobix $N $MAX_ITER $EPS s >> $RES_SEQ_FILE
     fi
     echo "Done"
     
-    echo "Running fast-flow"
+    echo "Running fastflow"
     echo $HEADER  > $RES_FF_FILE
     for i in 1 2 $(seq 4 $WORKER_STEPS $nw)
     do
 	echo -n "Working with $i ... "
 	if [[ "$1" == "MIC" ]]
 	then
-	    ssh mic1 "bin/jacobim $N $MAX_ITER $EPS f $i" >> $RES_FF_FILE
+	    ssh mic1 "./jacobim $N $MAX_ITER $EPS f $i" >> $RES_FF_FILE
 	else
 	    bin/jacobix $N $MAX_ITER $EPS f $i >> $RES_FF_FILE
 	fi
@@ -66,7 +66,7 @@ do
 	echo -n "Working with $i ... "
         if [[ "$1" == "MIC" ]]
 	then
-	    ssh mic1 "bin/jacobim $N $MAX_ITER $EPS p $i" >> $RES_PT_FILE
+	    ssh mic1 "./jacobim $N $MAX_ITER $EPS p $i" >> $RES_PT_FILE
 	else
 	    bin/jacobix $N $MAX_ITER $EPS p $i >> $RES_PT_FILE
         fi
