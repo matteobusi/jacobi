9a2a2f4e6668d9ee6e6d3eab0f6d8e5cd326c583
mcaos <caos750@gmail.com>
2017-06-11 18:04:46 +0200

    Modified py script to include stats

--- workspace/reportgen.py
+++ workspace/reportgen.py
@@ -8,10 +8,9 @@ import pandas as pd
 
 sns.set()
 sns.set(style="whitegrid")
-sns.set(font_scale=2) 
+sns.set(font_scale=2.5) 
 plt.rc('text', usetex=True)
-font = {'family' : 'Times-Roman',
-        'size'   : 22}
+font = {'family' : 'Times-Roman'}
 
 plt.rc('font', **font)
 color = sns.color_palette("Set2", 10)
@@ -35,6 +34,8 @@ for p in ["host", "mic"]:
             df_m['s'] = df_m.apply(lambda row: seq_time/row['latency'], axis=1)
             df_m['scalab'] = df_m.apply(lambda row: m_time_0/row['latency'], axis=1)
             df_m['eff'] = df_m.apply(lambda row: row['s']/row['nw'], axis=1)
+            
+            df_m['ratio'] =  df_m.apply(lambda row: row['sb_t']/row['comp_t'], axis=1)
 
             compl_seq = compl_seq.append(df_seq, ignore_index=True)
             compl_m = compl_m.append(df_m, ignore_index=True)
@@ -47,12 +48,28 @@ for p in ["host", "mic"]:
         maxW = compl_m['nw'].max()
 
         for (em, lbl) in [('s', 'Speedup'), ('scalab', 'Scalability'), ('eff', 'Efficiency')]:
-            g = sns.FacetGrid(compl_m, hue='N', aspect=1.6, size=11)
+            g = sns.FacetGrid(compl_m, hue='N', aspect=1.3, size=8)
             g.map(plt.scatter, 'nw', em)
             g.map(plt.plot, 'nw', em)
             g.add_legend()
-            g.set_xlabels("# Workers")
+            g.set_xlabels("Number of workers")
             g.set_ylabels(lbl)
             plt.savefig("graphs/graph_" + m + "_" + p + "_" + em + ".png")
 
-        # Compute the table and put into a file.
+        # Produce tables. 
+        # For each N and target processor compute 
+        #   - FF : best nw, latency (best), latency (1)
+        #   - TH : best nw, latency (best), latency(1), ratio
+        #   - Seq : latency
+        df_min = pd.DataFrame()
+        for n in [5000, 10000, 15000, 30000]:
+            df_1 = compl_m[compl_m['N'] == n ][ compl_m['nw'] == 1 ][['N', 'latency']]
+            df_best = compl_m[compl_m['N'] == n ]
+            df_best = df_best[ df_best['latency'] == df_best['latency'].min() ][['N', 'nw', 'latency', 'sb_t', 'comp_t', 'ratio']]
+            df_best['latency_1'] = df_best.apply(lambda row: df_1[df_1['N'] == row['N']]['latency'], axis=1)
+            df_min = df_min.append(df_best, ignore_index=True)
+            #print(df_best)
+
+        print("========== proc: {} = impl: {} ==========".format(p, m))
+        print(df_min)
+        print("==============================================")
